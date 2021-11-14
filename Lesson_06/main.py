22c1f46f2f79cb2981984de140eef1451d7c995d
unoplus <unoplus6t@gmail.com>
2021-11-14 15:15:44 -0800

    Parser template

---
+++ Lesson_06/main.py
@@ -0,0 +1,16 @@
+def parse_parameters(query: str) -> dict:
+    return {}
+
+
+def parse_cookies(query: str) -> dict:
+    return {}
+
+
+if __name__ == '__main__':
+    # Tests for function "parse_parameters"
+    assert parse_parameters('http://example.com/?') == {}
+    assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+
+    # Tests for function "parse_cookies"
+    assert parse_cookies('') == {}
+    assert parse_cookies('name=Dima;') == {'name': 'Dima'}
