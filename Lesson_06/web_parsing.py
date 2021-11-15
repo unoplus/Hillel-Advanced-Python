628f96c6478d3b9dee233112ec4acf8ad8015872
unoplus <unoplus6t@gmail.com>
2021-11-15 14:44:17 -0800

    Parser

--- Lesson_06/web_parsing.py
+++ Lesson_06/web_parsing.py
@@ -15,6 +15,7 @@ def parse_parameters(query: str) -> dict:
         params = dict(i.split('=') for i in param_parse.split('&'))
     else:
         params = dict(param_parse)
+
     return params
 
 
@@ -25,10 +26,11 @@ def parse_cookies(query: str) -> dict:
     :param query: Some cookies
     :return: Dictionary with parameters or empty.
     """
-    if query:
-        params = dict(i.split('=') for i in query.split(';'))
-    else:
-        params = dict(query)
+    params = {}
+    for item in query.split(';'):
+        params.update(i.split('=', 1) for i in item.split())
+    print(params)
+
     return params
 
 
@@ -39,24 +41,35 @@ if __name__ == '__main__':
         'https://docs.python.org/3?name=ferret&color=purple') == {
                'name': 'ferret', 'color': 'purple'}
     assert parse_parameters(
-        'https://docs.python.org/3?name=ferret') == {
+        'https://www.python.org/doc/?name=ferret') == {
                'name': 'ferret'}
     assert parse_parameters(
-        'https://docs.python.org/3?name=ferret&color=purple&login=admin') == {
+        'https://www.python.org/doc/?name=ferret&color=purple&login=admin') == {
                'name': 'ferret', 'color': 'purple', 'login': 'admin'}
     assert parse_parameters(
-        'https://docs.python.org/3?one=1&2=two') == {
+        'https://www.python.org/doc/?one=1&2=two') == {
                'one': '1', '2': 'two'}
 
     # Tests for function "parse_cookies"
     assert parse_cookies('') == {}
-    assert parse_cookies('name=Dima') == {'name': 'Dima'}
-    assert parse_cookies('name=Dima;alias=Dog;date=12.12.12') == \
-           {'name': 'Dima', 'alias': 'Dog', 'date': '12.12.12'}
-    assert parse_cookies('__utma=32101439.917134183.1636909172.1636909172.163;'
-                         '__utmc=32101439') == \
-           {'__utma': '32101439.917134183.1636909172.1636909172.163',
-            '__utmc': '32101439'}
+    assert parse_cookies('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookies('__utma=32101439.917134183.1636909172.1636909172.'
+                         '1636909172.1; __utmz=32101439.1636909172.1.1.utmcsr='
+                         'duckduckgo.com|utmccn=(referral)|utmcmd=referral|'
+                         'utmcct=/') == \
+           {'__utma': '32101439.917134183.1636909172.1636909172.1636909172.1',
+            '__utmz': '32101439.1636909172.1.1.utmcsr=duckduckgo.com|utmccn='
+                      '(referral)|utmcmd=referral|utmcct=/'}
+    assert parse_cookies('__utma=32101439.917134183.1636909172.1637010818.'
+                         '1637015905.3; __utmz=32101439.1636909172.1.1.utmcsr='
+                         'duckduckgo.com|utmccn=(referral)|utmcmd=referral|'
+                         'utmcct=/; __utmc=32101439;'
+                         '__utmb=32101439.2.10.1637015905; __utmt=1') == \
+           {'__utma': '32101439.917134183.1636909172.1637010818.1637015905.3',
+            '__utmz': '32101439.1636909172.1.1.utmcsr=duckduckgo.com|utmccn='
+                      '(referral)|utmcmd=referral|utmcct=/',
+            '__utmc': '32101439', '__utmb': '32101439.2.10.1637015905',
+            '__utmt': '1'}
     assert parse_cookies('_gcl_au=1.1.1269769116.1636900111;'
                          'PHPSESSID=mvrknprt8eu3p3dts0r5ab6rh2') == \
            {'_gcl_au': '1.1.1269769116.1636900111',
