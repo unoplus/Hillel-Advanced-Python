ebe6318c4a817f2a8904a9f21573d80e6bfb7f3e
unoplus <unoplus6t@gmail.com>
2021-12-06 23:32:48 -0800

    main flask file

--- HW_09/app.py
+++ HW_09/app.py
@@ -16,8 +16,7 @@ def main():
                ' указав количество;', 'средний вес и рост сферического коня в вакууме...(внезапно, да?)...;',
                'информацию о количестве астронавтов находящихся в космосе в данную минуту;',
                'закодировать любую строку в формате base58;', 'посмотреть на вашу строку в уже раскодированном виде...']
-    output = render_template('main.html', propose=propose)
-    return output
+    return render_template('main.html', propose=propose)
 
 
 @app.route('/requirements/')
@@ -25,20 +24,17 @@ def requirements():
     try:
         with open('requirements.txt', 'r') as file:
             some_text = file.read().split('\n')
-        output = render_template('requirements.html', some_list=some_text)
-        return output
+        return render_template('requirements.html', some_list=some_text)
     except FileNotFoundError as e:
         some_error = [e]
-        output = render_template('requirements.html', some_list=some_error)
-        return output
+        return render_template('requirements.html', some_list=some_error)
 
 
 @app.route('/generate-users/<int:number>')
 def generate_users(number):
     fake = Faker()
     fake_users = dict((fake.name(), fake.email()) for i in range(number))
-    output = render_template('generate-users.html', users=fake_users)
-    return output
+    return render_template('generate-users.html', users=fake_users)
 
 
 @app.route('/mean/')
@@ -56,34 +52,26 @@ def mean():
 
         mean = {'Parsed file:': 'hw05.csv', 'Total values in file (strings of data):': strings_of_data,
                 'Average height:': f'{2.54 * average_height} cm', 'Average weight:': f'{0.453592 * average_weight} kg'}
-        output = render_template('mean.html', mean=mean)
-        return output
+        return render_template('mean.html', mean=mean)
     except FileNotFoundError as e:
         mean = {'Parsed file:': e}
-        output = render_template('mean.html', mean=mean)
-        return output
+        return render_template('mean.html', mean=mean)
 
 
 @app.route('/space/')
 def space():
     r = requests.get('http://api.open-notify.org/astros.json')
     astronauts = r.json()['number']
-    output = render_template('space.html', number_of_astronauts=f'Now in space {astronauts} people')
-    return output
+    return render_template('space.html', number_of_astronauts=f'Now in space {astronauts} people')
 
 
 @app.route('/base58encode/<string:string>')
 def base58encode(string):
     base58encode = base58.b58encode(string)
-    output = render_template('base58encode.html', base58encode=base58encode)
-    return output
+    return render_template('base58encode.html', base58encode=base58encode)
 
 
 @app.route('/base58decode/<string:string_in_base58>')
 def base58decode(string_in_base58):
     base58decode = base58.b58decode(string_in_base58)
-    output = render_template('base58decode.html', base58decode=base58decode)
-    return output
-
-
-app.run(debug=True)
+    return render_template('base58decode.html', base58decode=base58decode)
