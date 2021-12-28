9def435a888ee3907b824aed1e10933df94fe07c
unoplus <87710423+unoplus@users.noreply.github.com>
2021-12-28 21:28:11 +0200

    Update settings.py

--- Lesson_21_part2/config/settings.py
+++ Lesson_21_part2/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^of&s8s5eiojkz6=ck8lurjkmeslci#u=a90zp2@_%b-l*qy)+'
+SECRET_KEY = 'django-insecure-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
